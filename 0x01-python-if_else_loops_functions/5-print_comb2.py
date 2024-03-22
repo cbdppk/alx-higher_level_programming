406ed31b3db6f3724811f2e15674134b2e154e57
cbdppk <dkjunior250@gmail.com>
2024-03-22 15:13:30 -0400

    loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if (i < 10):
-        print("{}{}, ".format("0", i), end="")
-    elif (i > 9 and i < 99):
-        print("{}, ".format(i), end="")
+    if (i < 98):
+        print("{0:02}, ".format(i), end="")
     elif (i > 98):
         print("{}".format(i))
