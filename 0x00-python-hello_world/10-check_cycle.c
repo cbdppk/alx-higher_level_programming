73b2da688b3e0172fb429181f0d30b0786be074c
danielk872 <juniordanielk872@gmail.com>
2024-04-23 20:14:42 -0500

    data

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * check_cycle - for checking if there is a cycle in a list
+ * @list: the head pointer
+ * Return: returns 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	if (list == NULL || list->next == NULL)
+		return (1);
+
+	slow = list;
+	fast = list->next;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
