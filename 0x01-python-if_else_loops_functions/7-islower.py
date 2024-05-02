5907d60c16c7764063bfb5a0142517f07551809e
danielk872 <juniordanielk872@gmail.com>
2024-05-02 17:09:03 -0500

    islowr

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z')+1):
-        if (c == chr(i)):
+        if (ord(c) < ord('a') or ord(c) > ord('z')):
+            return False
+        elif (c == chr(i)):
             return True
     return False
