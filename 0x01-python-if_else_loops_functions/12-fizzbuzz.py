296aacca3deaf72b82b1545344d0c6618c99a817
danielk872 <juniordanielk872@gmail.com>
2024-05-07 14:40:22 -0500

    loops

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,9 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if (i % 3 == 0):
+        if ((i % 3 == 0) && (i % 5 == 0)):
+            print("{}".format("FizzBuzz"), end=' ')
+        elif (i % 3 == 0):
             print("{}".format("Fizz"), end=' ')
         elif (i % 5 == 0):
             print("{}".format("Buzz"), end=' ')
